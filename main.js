be0b1153b7487f5738600e67d66c8a3bb30fdce9
takuto <takuto.yoshikai@gmail.com>
2020-05-28 12:41:07 +0900

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+let point = 0
+app.get("/", function(req, res, next) {
+  console.log("access!!!");
+  if (point === 0) {
+    res.json({ msg: "failed" })
+  } else {
+    res.json({ msg: "success"})
+    point -= 1
+  }
+})
+
+app.listen(3000, function() {
+  console.log("started")
+});
+
